231fb5d1b8520c72908e0d728d1380395b7e3be8
janskvara <jano.skvara@outlook.sk>
2021-10-26 10:17:15 -0700

    Initial Commit

---
+++ Tripie/TripieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripieApp.swift
+//  Tripie
+//
+//  Created by janoskvara on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct TripieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
