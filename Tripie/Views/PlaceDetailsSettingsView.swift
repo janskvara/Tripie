509bcbc0a4e3d0a00f42056ef3ab4c20541551fb
janskvara <jano.skvara@outlook.sk>
2021-12-18 05:02:10 -0800

    fix, that you are not able to add one place many times

--- Tripie/Views/PlaceDetailsSettingsView.swift
+++ Tripie/Views/PlaceDetailsSettingsView.swift
@@ -31,12 +31,13 @@ struct PlaceDetailsSettingsView: View {
                     Button(action: {addPlacesToFavorite()}){
                         Label("Favorite", systemImage: "heart")
                             .foregroundColor(.red)
-                    }
+                    }.disabled(isPlaceAleradyInFavoritePlaces())
+                    .opacity(isPlaceAleradyInFavoritePlaces() ? 0.3 : 1)
                     
                     Spacer()
                     Picker("Add to trip", selection: $selectedItem, content: {
                         Text("Add new trip").tag("Add new trip")
-                        ForEach(trips, id: \.self){trip in
+                        ForEach(trips.filter({_trip in return !places.contains(where: {_place in return _place.xid == place.properties.xid && _place.trip == _trip.name})}), id: \.self){trip in
                             Text(trip.name ?? "Unknown name").tag(trip.name ?? "Unknown name")
                         }
                     }).pickerStyle(MenuPickerStyle())
@@ -55,6 +56,9 @@ struct PlaceDetailsSettingsView: View {
                 }.padding()
             }
             AddTripAlert(title: "Add Trip", isShown: $isPresented, text: $text, onDone: { nameOfTrip in
+                if(trips.filter({trip in return trip.name == nameOfTrip}).count != 0){
+                    return
+                }
                 let trip = Trip(context: moc)
                 trip.id = UUID()
                 trip.name = nameOfTrip
@@ -63,17 +67,22 @@ struct PlaceDetailsSettingsView: View {
         }
     }
     
-    func addPlacesToFavorite(){
+    private func isPlaceAleradyInFavoritePlaces() -> Bool{
+        return places.filter({_place in _place.xid == place.properties.xid && _place.favorite}).count != 0
+    }
+
+    private func addPlacesToFavorite(){
         addPlaces(trip: "", favorite: true)
     }
     
-    func addPlaceToTrip(trip: String){
+    private func addPlaceToTrip(trip: String){
         addPlaces(trip: trip, favorite: false)
         
     }
     
-    func addPlaces(trip:String, favorite:Bool){
+    private func addPlaces(trip:String, favorite:Bool){
         let newPlace = Place(context: moc)
+        newPlace.xid = place.properties.xid
         newPlace.name = place.properties.name
         newPlace.dist = place.properties.dist
         newPlace.rate = Int32(place.properties.rate)
